5483f16323a33ad07b0a1ca2e1bc684de6a1cefd
chenmiaoi <3026066798@qq.com>
2023-03-26 01:43:06 +0800

    done the variables and read the chapter 3.1

--- exercises/variables/variables4.rs
+++ exercises/variables/variables4.rs
@@ -1,11 +1,12 @@
 // variables4.rs
 // Execute `rustlings hint variables4` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// I AM DONE
 
 fn main() {
     let x = 3;
     println!("Number {}", x);
+    let x: i32;
     x = 5; // don't change this line
     println!("Number {}", x);
 }
