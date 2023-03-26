1c59464ea12ec626f7a48e2f0e6ab9748841a57f
chenmiaoi <3026066798@qq.com>
2023-03-27 00:55:21 +0800

    done the move_semantics

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -2,12 +2,13 @@
 // Make me compile without changing line 13 or moving line 10!
 // Execute `rustlings hint move_semantics2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// I AM DONE
 
 fn main() {
     let vec0 = Vec::new();
 
-    let mut vec1 = fill_vec(vec0);
+    // 涉及到所有权的转移，我们并不想将vec0的所有权转交给vec1，因此需要借用
+    let mut vec1 = fill_vec(&vec0);
 
     // Do not change the following line!
     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
@@ -17,8 +18,11 @@ fn main() {
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 }
 
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
+// 因为传参是借用，因此标签也需要引用
+fn fill_vec(vec: &Vec<i32>) -> Vec<i32> {
+    // 但是我们发现，只是借用还是不行的，如果单纯的借用vec0，那么此处vec的类型是&Vec，这样就会导致返回值类型不一致
+    // 如果修改返回值类型为&Vec，就会导致后续的&mut，而后继续更改最终是悬垂引用，因此，干脆直接clone出一个变量转交，使其类型为Vec
+    let mut vec = vec.clone();
 
     vec.push(22);
     vec.push(44);
--- exercises/move_semantics/move_semantics5.rs
+++ exercises/move_semantics/move_semantics5.rs
@@ -3,13 +3,14 @@
 // adding, changing or removing any of them.
 // Execute `rustlings hint move_semantics5` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// I AM DONE
 
 fn main() {
+    // 不允许同时存在两个可变引用,且对其进行操作
     let mut x = 100;
     let y = &mut x;
-    let z = &mut x;
     *y += 100;
+    let z = &mut x;
     *z += 1000;
     assert_eq!(x, 1200);
 }
