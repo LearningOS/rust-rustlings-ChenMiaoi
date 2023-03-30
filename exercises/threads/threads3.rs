a3e853ebd0377e3159eba874ae0a7a3585821814
chenmiaoi <3026066798@qq.com>
2023-03-30 21:55:44 +0800

    done the thread

--- exercises/threads/threads3.rs
+++ exercises/threads/threads3.rs
@@ -1,7 +1,7 @@
 // threads3.rs
 // Execute `rustlings hint threads3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// I AM DONE
 
 use std::sync::mpsc;
 use std::sync::Arc;
@@ -28,6 +28,8 @@ fn send_tx(q: Queue, tx: mpsc::Sender<u32>) -> () {
     let qc = Arc::new(q);
     let qc1 = Arc::clone(&qc);
     let qc2 = Arc::clone(&qc);
+    // 多线程下，tx被一个线程所拥有
+    let tx1 = tx.clone();
 
     thread::spawn(move || {
         for val in &qc1.first_half {
@@ -40,7 +42,7 @@ fn send_tx(q: Queue, tx: mpsc::Sender<u32>) -> () {
     thread::spawn(move || {
         for val in &qc2.second_half {
             println!("sending {:?}", val);
-            tx.send(*val).unwrap();
+            tx1.send(*val).unwrap();
             thread::sleep(Duration::from_secs(1));
         }
     });
